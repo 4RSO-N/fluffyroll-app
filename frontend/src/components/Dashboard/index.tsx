d7d2daaeaf867f7b3b2f37df9aa0dc3d314febcb
4RSO-N <arsonild@outlook.com>
2025-10-07 13:40:05 +0100

    Create Dashboard components index

---
+++ frontend/src/components/Dashboard/index.tsx
@@ -0,0 +1,4 @@
+export { default as AnalyticsCard } from './AnalyticsCard';
+export { default as InsightCard } from './InsightCard';
+export { default as GoalCard } from './GoalCard';
+export { default as HealthScoreCard } from './HealthScoreCard';
